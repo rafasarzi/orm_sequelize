3b0743d469421c2264e0fa577af8a2056ca4ad04
rafasarzi <rajustino@gmail.com>
2022-04-08 16:06:10 -0300

    active record listing

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -27,6 +27,12 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     paranoid: true,
+    defaultScope: {
+      where: { ativo: true }
+    },
+    scopes: {
+      todos: { where: {} }
+    },
     modelName: 'Pessoas',
   });
   return Pessoas;

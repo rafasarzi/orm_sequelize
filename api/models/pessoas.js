dc2af43ebcc036e12217fa7431dab821b1c5db0f
rafasarzi <rajustino@gmail.com>
2022-04-08 17:40:24 -0300

    get all status 'confirmados' from a record

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,39 +1,44 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  class Pessoas extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      Pessoas.hasMany(models.Turmas, {
-        foreignKey: 'docente_id'
-      })
-      Pessoas.hasMany(models.Matriculas, {
-        foreignKey: 'estudante_id'
-      })
-
-    }
-  }
-  Pessoas.init({
-    nome: DataTypes.STRING,
+  const Pessoas = sequelize.define('Pessoas', {
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function(dado) {
+          if (dado.length < 3) throw new Error('o campo nome deve ter mais de 3 caracteres')
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'dado do tipo e-mail inválido'
+        }
+      }
+    },
     role: DataTypes.STRING
-  }, {
-    sequelize,
+  }, { 
     paranoid: true,
     defaultScope: {
       where: { ativo: true }
-    },
+    },  
     scopes: {
-      todos: { where: {} }
-    },
-    modelName: 'Pessoas',
-  });
-  return Pessoas;
-};
+      todos: { where: {} },
+      //etc: { constraint: valor }
+    }
+  })
+  Pessoas.associate = function(models) {
+    Pessoas.hasMany(models.Turmas, {
+      foreignKey: 'docente_id'
+    }) 
+    Pessoas.hasMany(models.Matriculas, {
+      foreignKey: 'estudante_id',
+      scope: { status: 'confirmado' },
+      as: 'aulasMatriculadas'
+    })
+  }
+  return Pessoas
+}

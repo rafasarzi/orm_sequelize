cca2d972d5b00a88f641fd5e1a5e2f14c034ffdf
rafasarzi <rajustino@gmail.com>
2022-04-11 11:29:02 -0300

    code rebilling with redirection to services

--- api/routes/turmasRoute.js
+++ api/routes/turmasRoute.js
@@ -1,15 +1,12 @@
-//routes/turmasRoute.js
-
 const { Router } = require('express')
 const TurmaController = require('../controllers/TurmaController')
-
+ 
 const router = Router()
-
-router.get('/turmas', TurmaController.pegaTodasAsTurmas)
-router.get('/turmas/:id', TurmaController.pegaUmaTurma)
-router.post('/turmas', TurmaController.criaTurma)
-router.put('/turmas/:id', TurmaController.atualizaTurma)
-router.delete('/turmas/:id', TurmaController.apagaTurma)
-router.post('/turmas/:id/restaura', TurmaController.restauraTurma)
-
+router
+  .get('/turmas', TurmaController.pegaTodasAsTurmas)  
+  .get('/turmas/:id', TurmaController.pegaTurma)  
+  .post('/turmas', TurmaController.criaTurma)  
+  .post('/turmas/:id/restaura', TurmaController.restauraTurma)  
+  .put('/turmas/:id', TurmaController.atualizaTurma)  
+  .delete('/turmas/:id', TurmaController.apagaTurma)  
 module.exports = router
